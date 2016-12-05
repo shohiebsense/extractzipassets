47c50befbae95701ea6e1cc94a044aa812353608
shohiebsense <shohieb.ahmad@gmail.com>
2016-12-05 11:04:28 +0700

    "zipUnzipppExample"

---
+++ app/src/main/java/com/shohiebsense/extractzipassets/MainActivity.java
@@ -0,0 +1,19 @@
+package com.shohiebsense.extractzipassets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Utils.unzip(this);
+    }
+
+
+
+
+
+}
